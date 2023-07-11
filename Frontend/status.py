d523b3432ceca9efe3a0c38a3a596d36ba113822
Eden Cohen <eededeedencohen@gmail.com>
2023-07-12 02:25:47 +0300

    implementing helper methos to set timestamp with param type str

--- Frontend/status.py
+++ Frontend/status.py
@@ -25,7 +25,7 @@ class Status:
             Returns True if the status is "done", False otherwise.
     """
 
-    def __init__(self, status: str, filename: str, timestamp: datetime, explanation: object) -> None:
+    def __init__(self, status: str, filename: str, timestamp: str, explanation: object) -> None:
         """
         @summary:
             Constructs a Status object.
@@ -40,8 +40,9 @@ class Status:
         """
         self.status = status
         self.filename = filename
-        self.timestamp = timestamp
+        self.timestamp = None
         self.explanation = explanation
+        self.set_datetime(timestamp)
 
     def is_done(self) -> bool:
         """
@@ -52,4 +53,14 @@ class Status:
         """
         return self.status == "done"
 
-
+    def set_datetime(self, string_datetime: str) -> None:
+        """
+        @summary:
+            Converts a string datetime to a datetime object and sets the timestamp attribute.
+        @param string_datetime:
+            str: The string datetime in this format:
+            'MM-DD-YYYY HH:MM'
+        @return:
+            None
+        """
+        self.timestamp = datetime.strptime(string_datetime, '%m-%d-%Y %H:%M')
