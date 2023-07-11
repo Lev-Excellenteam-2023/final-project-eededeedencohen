911ee835416ccc2bb8f88a80abea3533a3d74639
Eden Cohen <eededeedencohen@gmail.com>
2023-07-11 04:52:20 +0300

    adding extract data function from the filename

--- Explainer/process_new_pptx_files.py
+++ Explainer/process_new_pptx_files.py
@@ -1,6 +1,7 @@
 import asyncio
 import os
 from Explainer.convert_pptx_summary_to_json import convert_pptx_to_summary_and_write_to_json
+from Explainer.get_data_from_filename import get_unique_id, get_original_filename, get_uploaded_datetime
 
 
 async def process_pptx_file_to_json(filename: str) -> None:
@@ -12,6 +13,8 @@ async def process_pptx_file_to_json(filename: str) -> None:
         and then deletes the original file.
     @param filename:
         str: The name of the file to process.
+             The format of the filename should be:
+             <original_filename>__<uploaded_datetime>__<unique_id>.pptx
     @return:
         None
     """
@@ -19,15 +22,20 @@ async def process_pptx_file_to_json(filename: str) -> None:
 
     # Process only if the file is pptx file
     if os.path.isfile(uploaded_file_path) and filename.endswith(".pptx"):
+
+        # extract data from the filename
+        original_filename = get_original_filename(filename)
+        unique_id = get_unique_id(filename)
+        uploaded_datetime = get_uploaded_datetime(filename)
+
         output_filename = filename.replace(".pptx", "") + ".json"
         output_file_path = "../Shared/outputs/" + output_filename
-        title_name = filename.replace(".pptx", "")
+        title_name = original_filename
 
         await convert_pptx_to_summary_and_write_to_json(uploaded_file_path, output_file_path, title_name)
+        os.remove(uploaded_file_path)
 
-        input_filename = filename.replace(".pptx", "")
-        os.remove("../Shared/uploads/" + input_filename + ".pptx")
-        print(os.listdir("../Shared/outputs/"))
+        print(f"The file {original_filename}.pptx with the unique id: {unique_id} witch uploaded at {uploaded_datetime} was processed successfully.\n")
 
 
 async def process_new_uploaded_files_to_json() -> None:
@@ -43,10 +51,15 @@ async def process_new_uploaded_files_to_json() -> None:
         new_uploaded_filenames = os.listdir("../Shared/uploads")
 
         # Gather tasks and run them in parallel
+        print("Processing new uploaded files...")
         tasks = [process_pptx_file_to_json(file) for file in new_uploaded_filenames]
+        if not tasks:
+            print("No new uploaded files.")
         await asyncio.gather(*tasks)
 
         # Wait 10 seconds
+        print("Waiting 10 seconds...")
+        print("===============================================================================")
         await asyncio.sleep(10)
 
 asyncio.run(process_new_uploaded_files_to_json())
